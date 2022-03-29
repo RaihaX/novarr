086dc5b2f68059e4c470f141c882fff92a3b9f3d
Reyhan Thee <rey@visns.studio>
2022-03-30 07:21:51 +0800

    update

--- app/Language.php
+++ app/Language.php
@@ -2,10 +2,13 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Language extends Model
 {
+    use HasFactory;
+    
     public function novels() {
         return $this->hasMany('App\Novel');
     }
